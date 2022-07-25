fd660c9340f5308dd6082c5833b1f7e3084bbcbb
Sachmilan <sachmilansingh@gmail.com>
2022-07-25 05:55:29 -0600

    pushing the files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-// Linking to the HTML document and storing these elements as variables
 const cityInputEl = $("#city-input");
 const historyEl = $("#history");
 const cityNameEl = $("#city-name");
@@ -9,23 +8,23 @@ const temperatureEl = $("#temperature");
 const humidityEl = $("#humidity");
 const windSpeedEl = $("#wind-speed");
 const indexUVEl = $("#UV-index");
-const fiveDayForecastEl = $("#five-day-forecast-boxes");
+const futureForecast = $("#five-day-forecast-boxes");
 
-// Links to the buttons in the HTML document, stored as variables
+// Links to the buttons 
 const searchBtn = $("#search-button");
 const clearBtn = $("#clear-history-button");
 
 // unique API key for the OpenWeather API. Please don't use :)
-const apiKey = "17e28f443188585122fb7d4ef35559f9";
+const apiKey = "1291db61f641ff72ce7519899878bf5a";
 
 // creating variables to store an interval counter and user's searched cities
 let setDateTime = null;
 let savedCities = JSON.parse(localStorage.getItem("city-search-history")) || [];
 
-// function to fetch a city's weather based on the user's input on the webpage
-const fetchCity = (event) => {
+// will bring the weather information based on the user input
+const mainCity = (event) => {
 
-    // prevents the form from refreshing the page
+    // stops the form from refreshing the page
     event.preventDefault();
 
     // store the user's input in a variable, then use it for the queryURL
@@ -66,14 +65,13 @@ const fetchCity = (event) => {
 
 };
 
-// function that does the same thing as fetchCity but is triggered from the user's saved searches
+// function that does the same thing as mainCity but is triggered from the user's saved searches
 // on the page
-const fetchCityFromHistory = (event) => {
+const oldCity = (event) => {
 
     event.preventDefault();
 
     // user's searches are saved as buttons with a value. this stores the value of the button
-    // (previously searched city) that the user clicked as a variable
     const city = $(event.target).val();
 
     // queryURL
@@ -88,34 +86,6 @@ const fetchCityFromHistory = (event) => {
 
 };
 
-// When the page first loads, Open Weather is queried for Toronto's weather data, and displays
-// it to the page. This function starts that process and is called by the init function
-// I chose Toronto as the first thing the user sees just as an example of what the page will
-// display, rather than an empty page with a single search bar. Toronto is the nearest major
-// city to my location. Does the same thing as fetchCity and fetchCityFromHistory functions
-// but the city is set to Toronto
-const fetchToronto = () => {
-
-    const city = "Toronto";
-    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}`;
-
-    fetch(weatherURL)
-        .then(function (response) {
-
-            // if for some reason, the response doesn't return a 200s status, a message
-            // displays on the page as an alert. The query might still work, just not
-            // return a 200s status
-            if (response.status < 200 || response.status >= 300) {
-                window.alert("Something may have gone wrong loading default data to this page.");
-                return;
-            }
-
-            return response.json();
-
-        })
-        .then(renderWeather);
-
-};
 
 // function that interprets the data returned from the above 3 functions and displays specific 
 // information on the page
@@ -133,11 +103,7 @@ const renderWeather = (data) => {
     const lat = data.coord.lat;
     const lon = data.coord.lon;
 
-    // some of the information is displayed in different units, so we call a few functions
-    // that calculates that information into the units we want. Just as a note, the query
-    // URL can include parameters that asks for the information returned as a certain type of
-    // unit. I just did not include those parameters in the call. Check OpenWeather's
-    // documentation for more information
+   // Converting the units
     const celsius = kelvinToCelsius(kelvin);
     const fahrenheit = kelvinToFahrenheit(kelvin);
     const mph = metersPerSecondToMPH(windMPS);
@@ -180,24 +146,19 @@ const fetchForecast = (lat, lon) => {
         .then(renderForecast);
 };
 
-// function that interprets the data from fetchForecast, extracts specific data, and displays
-// it on the page
+// extracts data
 const renderForecast = (data) => {
     
-    // stores two things as variables. First the timezone offset to calculate the correct dates
-    // for the 5-day forecase, and second the list of daily forecasts obtained from the response
     const forecastTimezoneOffset = data.timezone_offset;
     const forecastList = data.daily;
 
-    // ensures the five-day forecast section in the HTML is empty, because we'll be appending new
-    // data to it
-    fiveDayForecastEl.empty();
+    //checks if the future elements are empty
+    futureForecast.empty();
 
-    // use a for loop to only interpret the data for the next 5 days. I chose not to display data for
-    // the current day, which is why i = 1, because I thought it would be redundant.
+    // for future.
     for (let i = 1; i < 6; i++) {
 
-        // extracts and save specifc information from the return as variables for readability
+        // extracts information and save it into variables
         const forecastTempK = forecastList[i].temp.day;
         const forecastHumidity = forecastList[i].humidity;
         const forecastWind = forecastList[i].wind_speed;
@@ -210,8 +171,7 @@ const renderForecast = (data) => {
         const forecastTempF = kelvinToFahrenheit(forecastTempK);
         const forecastWindMPH = metersPerSecondToMPH(forecastWind);
 
-        // create the container and elements inside the container that will make up each individual
-        // forecast. incorporate bootstrap classes here for styling 
+        // creating the containers 
         const forecastBox = $('<div class="five-day-forecast-box pt-2 col-lg-2 bg-primary text-white m-2 rounded">');
         const dateP = $("<p>");
         const weatherIconImg = $("<img>");
@@ -219,7 +179,7 @@ const renderForecast = (data) => {
         const humidityP = $("<p>");
         const windP = $("<p>");
 
-        // plugs the data extracted into the correct elements
+        // pushes the data into specific places
         dateP.text(forecastDate);
         weatherIconImg.attr("src", `https://openweathermap.org/img/wn/${forecastIcon}@2x.png`);
         weatherIconImg.attr("alt", forecastDescription);
@@ -230,7 +190,7 @@ const renderForecast = (data) => {
 
         // append the box to the forecast section of the HTML doc, then append the rest of the data
         // to the box
-        fiveDayForecastEl.append(forecastBox);
+        futureForecast.append(forecastBox);
         forecastBox.append(dateP);
         forecastBox.append(weatherIconImg);
         forecastBox.append(tempP);
@@ -244,21 +204,20 @@ const renderForecast = (data) => {
 // chain of functions called after the user searches for a city
 const fetchUV = (lat, lon) => {
 
-    // this function uses latitude and longitude data for the query URL, and ends in a call of the
-    // renderUV function
+    //uses the latitude and longitude
     const uvURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     fetch(uvURL)
         .then(function (response) {
             return response.json();
         })
-        .then(renderUV)
+        .then(uvData)
 
 };
 
 // render function that takes the data fetched using fetchUV function, interpets it and displays
 // it on the page
-const renderUV = (data) => {
+const uvData = (data) => {
 
     // saves the uv index as a variable
     const uv = data.current.uvi;
@@ -269,8 +228,7 @@ const renderUV = (data) => {
     // ensures the uv index element in the HTML is empty since we'll be appending new information
     indexUVEl.empty();
 
-    // these statements interpret the uv index and gives the span created above a color and some
-    // information that the user can see on hover related to how high the uv index is
+    // changes the color according to the uv index
     if (uv < 3) {
         uvValueEl.addClass("bg-success text-white");
         uvValueEl.attr("title", "Low");
@@ -296,28 +254,22 @@ const renderUV = (data) => {
     indexUVEl.append(uvValueEl);
 };
 
-// this function calculates the temperature in Fahrenheit from a temperature in Kelvin. rounds to
-// 2 decimal places
+// converts the temperature unit
 const kelvinToFahrenheit = (K) => {
     return Number((K - 273.15) * (9/5) + 32).toFixed(2);
 };
 
-// this function calculates the temperature in Celsius from a temperature in Kelvin. rounds to
-// 2 decimal places
+// converts the temperature units
 const kelvinToCelsius = (K) => {
     return Number(K - 273.15).toFixed(2);
 };
 
-// this function calculates the wind speed in miles per hr given a wind speed in meters/second. 
-// rounds to 2 decimal places
+// measures wind speed
 const metersPerSecondToMPH = (m) => {
     return Number(m * 2.237).toFixed(2);
 };
 
-// function that calculates the local date given the timezone offset data from the fetch response
-// hours parameter denotes how many hours after local time we want to return. used for the 5-day 
-// forecast dates in the renderForecast function. basically will return the next 5 days from the 
-// local date. Uses Moment.js
+// local time
 const calculateForecastDate = (timezoneOffset, hours) => {
     
     // UTC time 
@@ -374,11 +326,11 @@ const saveHistory = (cityName, countryName) => {
         country: countryName
     };
 
-    // checks if a search has already been stored in localStorage using the checkDuplicate
+    // checks if a search has already been stored in localStorage using the preventDuplicate
     // function and stored in a variable for readability
-    const duplicateSave = checkDuplicate(newSearch, savedCities);
+    const duplicateSave = preventDuplicate(newSearch, savedCities);
 
-    // if the result of the checkDuplicate function is true (city had already been saved in
+    // if the result of the preventDuplicate function is true (city had already been saved in
     // localStorage), then function exits
     if (duplicateSave) {
         return;
@@ -413,9 +365,7 @@ const renderHistory = () => {
 
     for (let i = 0; i < savedCities.length; i++) {
 
-        // creates a button (and styles them with bootstrap classes) for each saved search. Also
-        // gives each button a class and value that helps denote them as a search history button
-        // and for easier extraction of the city each button is associated with
+        // makes buttom for the history and make them clickable
         const createBtn = $(`<button class="history-btn list-group-item list-group-item-action text-center" value="${savedCities[i].city}, ${savedCities[i].country}">`);
         createBtn.text(`${savedCities[i].city}, ${savedCities[i].country}`);
 
@@ -449,14 +399,12 @@ const clearHistory = () => {
 
 };
 
-// function used by the saveHistory function. checks of an object already exists in a list
-// to prevent a duplicate push to the savedCities array (and a duplicate add to localStorage)
-const checkDuplicate = (obj, list) => {
+// prevents duplicates
+const preventDuplicate = (obj, list) => {
 
     for (let i = 0; i < list.length; i++) {
 
-        // checks if the object (which has the city and country properties from the user's search)
-        // already exists in the savedCities array. if yes, return true
+        // verify the search
         if (JSON.stringify(list[i]) === JSON.stringify(obj)) {
 
             return true;
@@ -483,9 +431,9 @@ const init = () => {
 
 // click event listeners for the search button, clear button, and history buttons in the search
 // history box and their respective function calls
-searchBtn.click(fetchCity);
+searchBtn.click(mainCity);
 clearBtn.click(clearHistory);
-historyEl.on("click", ".history-btn", fetchCityFromHistory);
+historyEl.on("click", ".history-btn", oldCity);
 
 // call init function on page load
 init();
